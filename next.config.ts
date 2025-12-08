637848137ca455f3e55574eb9787d3713ffc3221
unknown <afonsomiguel1990@gmail.com>
2025-12-08 23:50:35 +0000

    fix: expose FIREBASE_WEBAPP_CONFIG in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -12,6 +12,9 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "storage.googleapis.com" },
     ],
   },
+  env: {
+    FIREBASE_WEBAPP_CONFIG: process.env.FIREBASE_WEBAPP_CONFIG,
+  },
 };
 
 export default nextConfig;
